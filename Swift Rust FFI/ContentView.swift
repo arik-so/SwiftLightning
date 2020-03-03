4b612dd908b2bea9291b62f09902def4f2938444
Arik Sosman <git@arik.io>
2020-03-03 01:12:25 -0800

    Initial Commit

---
+++ Swift Rust FFI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Rust FFI
+//
+//  Created by Arik Sosman on 3/3/20.
+//  Copyright © 2020 Arik Sosman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Rust FFITests/Swift_Rust_FFITests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift_Rust_FFITests.swift
+//  Swift Rust FFITests
+//
+//  Created by Arik Sosman on 3/3/20.
+//  Copyright © 2020 Arik Sosman. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_Rust_FFI
+
+class Swift_Rust_FFITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
